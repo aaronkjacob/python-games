2856b2b37bc7fae60fc08ddc8a29f1cff294eb5a
Aaron Jacob <aaron.kj9@gmail.com>
2025-07-14 21:09:54 -0400

    Coding

--- Platformer/code/main.py
+++ Platformer/code/main.py
@@ -19,10 +19,28 @@ FPS = 60  # Frames per second
 
 PLAYER_VELOCITY = 5  # Player movement speed
 
+def get_background(name):
+    # Load background image
+    image = pygame.image.load(join("assets", "backgrounds", name))
+    __, __, width, height = image.get_rect()
+    tiles = []
 
+    for i in range(SCREEN_WIDTH // width + 1):
+        for j in range(SCREEN_HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+    return tiles, image
+
+def draw(screen,background, bg_image):
+    for tile in background:
+        screen.blit(bg_image, tile)
+    pygame.display.update()
 
 def main(screen):
     clock = pygame.time.Clock() # Initialize the clock
+
+    background, bg_image = get_background("Blue.png")  # Load the background image
+
     running = True # Main game loop
     while running:
         for event in pygame.event.get():
@@ -34,7 +52,7 @@ def main(screen):
 
         # Fill the screen with the background color
         screen.fill(BG_COLOR)
-
+        draw(screen,background,bg_image)
 
         # Update the display
         pygame.display.flip()

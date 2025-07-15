71b750f5ed5dec1e2076694e5c4f6b518897c896
Aaron Jacob <aaron.kj9@gmail.com>
2025-07-15 13:45:22 -0400

    Draw player sprite on screen

--- Platformer/code/main.py
+++ Platformer/code/main.py
@@ -19,8 +19,35 @@ FPS = 60  # Frames per second
 
 PLAYER_VELOCITY = 5  # Player movement speed
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites] # Flip sprites horizontally
+def load_spritesheet(dir1,dr2,width,height,direction=False):
+    path = join("Platformer", "assets", dir1, dr2)
+    images = [f for f in listdir(path) if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+        
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+        if direction:
+            all_sprites[image.replace('.png','') + '_right'] = sprites
+            all_sprites[image.replace('.png','') + '_left'] = flip(sprites)
+        else:
+            all_sprites[image.replace('.png','')] = sprites
+    return all_sprites
+
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)  # Player color (red)
+    GRAVITY = 1
+    SPRITES = load_spritesheet("MainCharacters", "MaskDude", 32, 32, True) # Load player spritesheet
+
     def __init__(self, x,y,width,height):
         self.rect = pygame.Rect(x, y, width, height)
         self.x_vel = 0
@@ -28,6 +55,7 @@ class Player(pygame.sprite.Sprite):
         self.mask = None
         self.direction = 'left'
         self.animation_count = 0
+        self.fall_count = 0
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
@@ -42,9 +70,13 @@ class Player(pygame.sprite.Sprite):
             self.direction = 'right'
             self.animation_count = 0
     def loop(self,fps):
+        #self.y_vel += min(1, (self.fall_count / fps))  * self.GRAVITY  # Apply gravity
         self.move(self.x_vel, self.y_vel)
+
+        self.fall_count += 1
     def draw(self, screen):
-        pygame.draw.rect(screen, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0] # Default sprite
+        screen.blit(self.sprite, (self.rect.x, self.rect.y))  # Draw the player sprite
 
 
 def get_background(name):

97589676057255b8be93380cf2f9ba08c7f7a127
Aaron Jacob <aaron.kj9@gmail.com>
2025-07-16 18:44:22 -0400

    player init setup

--- Space invaders/code/main.py
+++ Space invaders/code/main.py
@@ -8,6 +8,12 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
 
+class Player(pygame.sprite.Sprite):
+  def __init__(self,x,y):
+    super().__init__()
+    self.x = x
+    self.y = y
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -16,7 +22,7 @@ while running:
 
     screen.fill((0, 0, 0))
 
-    
+
 
     pygame.display.update()
     clock.tick(60)

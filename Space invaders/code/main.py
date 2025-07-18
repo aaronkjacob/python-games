4dda2ba2f35e9e07c0400bb006fe429072561166
Aaron Jacob <aaron.kj9@gmail.com>
2025-07-18 08:19:00 -0400

    Make bullet delay and more realistic shooting for space invaders

--- Space invaders/code/main.py
+++ Space invaders/code/main.py
@@ -2,8 +2,8 @@ import pygame
 import sys
 
 pygame.init()
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 700
+SCREEN_WIDTH = 1300
+SCREEN_HEIGHT = 900
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
@@ -24,6 +24,9 @@ class Player(pygame.sprite.Sprite):
     self.rect.x += self.vx
     self.draw()
 
+bullet_delay_count = 0
+bullet_delay = 30
+
 class PlayerBullet(pygame.sprite.Sprite):
   def __init__(self):
     super().__init__()
@@ -31,44 +34,47 @@ class PlayerBullet(pygame.sprite.Sprite):
     self.y = player.rect.top
     self.vy = 0
     self.speed = 20
-    self.delay = 0
   def draw(self):
-    self.delay += 1
     pygame.draw.circle(screen, 'white', (self.x,self.y), 5)
     self.vy = self.speed
     self.y -= self.vy
     if self.y < 0:
       self.kill()
-    
+      playerBullets.remove(self)
 
-player = Player(SCREEN_WIDTH/2, 600)
+player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT-100)
 playerBullets = []
 
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+      if event.type == pygame.QUIT:
+          running = False
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-      playerBullets.append(PlayerBullet())
     if keys[pygame.K_a]:
       player.vx = -player.speed
     if keys[pygame.K_d]:
       player.vx = player.speed
-    
     if (not keys[pygame.K_a]) and (not keys[pygame.K_d]):
       player.vx = 0
 
+    bullet_delay_count+=1
+
     screen.fill((0, 0, 0))
 
+    collision_rect = pygame.draw.rect(screen, 'white', (SCREEN_WIDTH/2,100,50,50))
+
     player.update()
+
+    if bullet_delay_count > bullet_delay:
+      playerBullets.append(PlayerBullet())
+      bullet_delay_count = 0
 
 
     for bullet in playerBullets:
       bullet.draw()
-      bullet.delay = 0
-
+      if collision_rect.collidepoint(bullet.x, bullet.y):
+        pygame.draw.rect(screen, 'red', (SCREEN_WIDTH/2,100,50,50))
 
     pygame.display.update()
     clock.tick(60)

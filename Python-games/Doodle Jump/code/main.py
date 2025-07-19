11e3d54ce5105fa4a302798a2765cccaeb4ebac4
Aaron Jacob <aaron.kj9@gmail.com>
2025-07-19 19:21:02 -0400

    Initialise Doodle jump game

--- Python-games/Doodle Jump/code/main.py
+++ Python-games/Doodle Jump/code/main.py
@@ -1,15 +1,13 @@
 #import necessary modules
 import pygame
-from pygame.locals import *
-from input import handle_input
 
 # Initialize Pygame
 pygame.init()
 
 # Set up window dimensions
-WIDTH, HEIGHT = 1000, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Platformer Game")
+SCREEN_WIDTH, SCREEN_HEIGHT = 1000, 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Jumpy")
 
 # Running variable and clock
 clock = pygame.time.Clock()
@@ -17,7 +15,7 @@ running = True
 
 while running:
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             running = False
 
     # Fill the screen with a color (e.g., black)

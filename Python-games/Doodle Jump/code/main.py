5cb60d0a6f34ca2f0e7227e17f8e82f249c7717c
Aaron Jacob <aaron.kj9@gmail.com>
2025-07-19 19:55:13 -0400

    Updating player rectangle

--- Python-games/Doodle Jump/code/main.py
+++ Python-games/Doodle Jump/code/main.py
@@ -21,11 +21,15 @@ bg_image = pygame.image.load('Python-games/Doodle Jump/Assets/bg.png').convert_a
 # Player class
 class Player():
     def __init__(self, x, y):
-        self.image = jumpy_image
-        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(jumpy_image, (45, 45))
+        self.width = 25
+        self.height = 40
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = (x,y)
-    def draw(self):
-        screen.blit(self.image, self.rect)
+    def draw(self):        
+        screen.blit(self.image, (self.rect.x - 12, self.rect.y - 5))
+        pygame.draw.rect(screen, 'black', self.rect, 2)
+
 
 jumpy = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT - 150)
 

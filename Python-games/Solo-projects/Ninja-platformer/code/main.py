966409d12cb8a0fddf72b47289eac6c6149323ba
Aaron Jacob <aaron.kj9@gmail.com>
2025-07-23 17:33:07 -0400

    make idle animation for player

--- Python-games/Solo-projects/Ninja-platformer/code/main.py
+++ Python-games/Solo-projects/Ninja-platformer/code/main.py
@@ -26,13 +26,17 @@ class Player(pygame.sprite.Sprite):
     super().__init__()
     self.x = x
     self.y = y
-    self.image = pygame.image.load('Python-games/Solo-projects/Ninja-platformer/assets/player/Ninja1.png')
+    self.animation_count = 1
+    self.animage_frame = [0,1,2,3]
+    self.image = pygame.image.load('Python-games/Solo-projects/Ninja-platformer/assets/player/idle/Ninja1.png')
     self.rect = pygame.Rect(x,y,self.image.get_width(),self.image.get_height())
     self.speed = 10
     self.flip = True
-    self.gravity_speed = 5
     self.jump_count = 0
     self.vel_y = 0
+    self.animation_speed = .1
+    self.idle = True
+    self.moving = False
   def move(self):
     dx = 0
     dy = 0
@@ -42,9 +46,16 @@ class Player(pygame.sprite.Sprite):
     if keys[pygame.K_d]:
       dx = self.speed
       self.flip = False
-    if keys[pygame.K_a]:
+      self.moving = True
+      self.idle = False
+    elif keys[pygame.K_a]:
       dx = -self.speed
       self.flip = True
+      self.moving = True
+      self.idle = False
+    else:
+      self.moving = False
+      self.idle = True
 
     self.vel_y += GRAVITY
     dy += self.vel_y
@@ -61,11 +72,17 @@ class Player(pygame.sprite.Sprite):
 
     self.rect.x += dx
     self.rect.y += dy
-
-    print(dy)
   def draw(self):
+    # animate and draw character
+    if self.animation_count > 4: # if the animatoin count is greater than the number of frames then reset the frames
+      self.animation_count = 1
+    if self.idle == True:
+      self.image = pygame.image.load('Python-games/Solo-projects/Ninja-platformer/assets/player/idle/Ninja' + str(int(self.animation_count)) + '.png') # load the image directer with the string form if the integer of the animatoin count for the image frame changes
+    else:
+      self.image = pygame.image.load('Python-games/Solo-projects/Ninja-platformer/assets/player/idle/Ninja1.png')
     screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
     pygame.draw.rect(screen, 'white', self.rect, 2)
+    self.animation_count += self.animation_speed
 
 # create an instance of player
 player = Player(SCREEN_WIDTH/2, 0)
